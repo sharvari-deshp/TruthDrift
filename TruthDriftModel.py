535867aa53b45b7e81ee9b2c122b0d29b44ffde8
Sharvari Deshpande <sharvari1.deshpande@gmail.com>
2026-04-21 19:39:26 -0400

    imported dataset

--- TruthDriftModel.py
+++ TruthDriftModel.py
@@ -1,4 +1,15 @@
 #TruthDrift: An ML model designed to detect hallucinations within AI generated text. 
+import json
 
-from datasets import load_dataset
-feverFile = load_dataset("json", data_files="train.jsonl", trust_remote_code=True)
+feverFile = []
+with open("FEVER Dataset JSON Files/train (1).jsonl", "r") as parse:
+    for line in parse: 
+        text = json.loads(line) #converts to dict type
+
+        if (text['label'] != "NOT ENOUGH INFO"):
+            feverFile.append(text)
+            
+        
+#test if dataset is imported
+print(feverFile[0])
+print(len(feverFile))
